87af8313c5ba55329b2d26683c25223a2a8497ce
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-07-31 17:48:34 -0700

    start working

--- src/app.js
+++ src/app.js
@@ -10,28 +10,8 @@ import About from './common/about';
 class App extends Component {
     render(){
         return (
-          <div>
+          <div className="App">
             <div className="container">
-              <nav className="navbar navbar-default">
-                <div className="container-fluid">
-                  <div className="navbar-header">
-                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                      <span className="sr-only">Toggle navigation</span>
-                      <span className="icon-bar"></span>
-                      <span className="icon-bar"></span>
-                      <span className="icon-bar"></span>
-                    </button>
-                    <a className="navbar-brand" href="#">React Router 4</a>
-                  </div>
-                  <div id="navbar" className="navbar-collapse collapse">
-                    <ul className="nav navbar-nav">
-                      <li className="active"><NavLink to="/">Home</NavLink></li>
-                      <li><NavLink to="/submit">Submit a Recipe</NavLink></li>
-                    </ul>
-                  </div>
-                </div>
-              </nav>
-           </div> 
           </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 // App modules
 import Submit from './common/submit';
@@ -10,15 +10,29 @@ import Home from './common/home';
 
 ReactDOM.render(
     <Router>
-        <div>
-          <ul>
-            <li> <Link to="/">Home</Link></li>
-            <li> <Link to="/submit">Submit</Link></li>
-          </ul>
-
-          <Route exact path="/" component={Home}/>
-          <Route path="/submit" component={Submit}/>
-        </div>
-    </Router>,
+         <div className="container">
+              <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                  <div className="navbar-header">
+                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                      <span className="sr-only">Toggle navigation</span>
+                      <span className="icon-bar"></span>
+                      <span className="icon-bar"></span>
+                      <span className="icon-bar"></span>
+                    </button>
+                    <a className="navbar-brand" href="#">React Router 4</a>
+                  </div>
+                  <div id="navbar" className="navbar-collapse collapse">
+                    <ul className="nav navbar-nav">
+                      <li className="active"><NavLink to="/">Home</NavLink></li>
+                      <li><NavLink to="/submit">Submit a Recipe</NavLink></li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+            <Route exact path="/" component={Home}/>
+            <Route path="/submit" component={Submit}/>
+          </div> 
+     </Router>,
     document.getElementById('app')
 );
